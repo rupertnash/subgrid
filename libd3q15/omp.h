62d34a917a61d4acf35c6a45a444ca4c802894a6
Rupert Nash <rupert.nash@ed.ac.uk>
2014-10-09 17:59:06 +0100

    Correct OpenMP header

--- libd3q15/omp.h
+++ libd3q15/omp.h
@@ -1,5 +1,7 @@
-#ifdef _OPENMP
+#ifndef DQ_OMP
+#define DQ_OMP
 
+#ifdef _OPENMP
 #include <omp.h>
 
 #else
@@ -7,5 +9,7 @@ typedef int omp_int_t;
 inline omp_int_t omp_get_num_threads() { return 1;}
 inline omp_int_t omp_get_thread_num() { return 0;}
 inline omp_int_t omp_get_max_threads() { return 1;}
+
+#endif
 
 #endif

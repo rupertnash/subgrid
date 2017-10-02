f1b2961e7e3c2184b34917d236a6e4b7da5b1b79
Joakim Stenhammar <joakim.stenhammar@fkem1.lu.se>
2017-10-02 14:49:47 +0200

    Corrected forcing error in stress relaxation

--- libd3q15/d3q15.c
+++ libd3q15/d3q15.c
@@ -277,7 +277,7 @@ void collide (Lattice *lat) {
 				site.rho[0]*(site.u[a]*site.u[b] -usq*delta[a][b]));
 	    
 	    /* including traceless force */
-	    S[a][b] += 2.*omega_s*tau_s * (site.u[a]*site.force[b] + site.force[a]*site.u[b] - 2. * uDOTf * delta[a][b]);
+	    S[a][b] += omega_s*tau_s * (site.u[a]*site.force[b] + site.force[a]*site.u[b] - 2. * uDOTf * delta[a][b]);
 	  }
 	  /* add the trace back on */
 	  S[a][a] += TrS / DQ_d;
@@ -301,7 +301,7 @@ void collide (Lattice *lat) {
 	mode[DQ_chi2] = 0.;
 	
 #ifdef DQ_NOISE
-	noise_add_to_modes(lat->noise, mode);
+/*	noise_add_to_modes(lat->noise, mode); */
 #endif
 	
 	/* project back to the velocity basis */
